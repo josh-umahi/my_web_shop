4467c632ad3dce78bb91350c5e78374c663f4c0b
josh-umahi <thejoshumahi@gmail.com>
2022-06-09 11:33:54 -0700

    Implemented a "Payment successful" page

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -6,12 +6,12 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
+        <p className="banner-product">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
         <img
           src={urlFor(heroBanner.image)}
-          alt="headphones"
+          alt="products"
           className="hero-banner-image"
         />
 
