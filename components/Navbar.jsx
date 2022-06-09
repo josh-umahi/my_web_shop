e9ed742a752ee520aa5b03eb8d6af582db1e1d74
josh-umahi <thejoshumahi@gmail.com>
2022-06-08 23:51:12 -0700

    Implemented Stripe for payments

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">JSM Headphones</Link>
+        <Link href="/">My Web Shop</Link>
       </p>
 
       <button
