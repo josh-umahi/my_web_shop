70efb4ca9eb491a912be77ff42d532921d97c70b
josh-umahi <thejoshumahi@gmail.com>
2022-06-10 14:38:20 -0700

    Few minor changes

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ const Home = ({ products, bannerData }) => (
   <div>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
     <div className="products-heading">
-      <h2>Best Seller Products</h2>
-      <p>speaker There are many variations passages</p>
+      <h2>Trending Now</h2>
+      <p>Our best selling products of the month</p>
     </div>
 
     <div className="products-container">
